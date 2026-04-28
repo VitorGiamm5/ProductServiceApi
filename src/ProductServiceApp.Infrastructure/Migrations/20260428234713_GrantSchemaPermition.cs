8678c2e38173b11a9e6619a37e9de5dcfd2b1396
Vitor <vitorgiammellia@movida.com.br>
2026-04-28 20:49:42 -0300

    chorus: grand permission schema in migration

---
+++ src/ProductServiceApp.Infrastructure/Migrations/20260428234713_GrantSchemaPermition.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductServiceApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GrantSchemaPermition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                DO $$
+                BEGIN
+                    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'read_randandan') THEN
+                        GRANT USAGE ON SCHEMA "dbSchemaGoodHamburger" TO read_randandan;
+                        GRANT SELECT ON ALL TABLES IN SCHEMA "dbSchemaGoodHamburger" TO read_randandan;
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA "dbSchemaGoodHamburger" GRANT SELECT ON TABLES TO read_randandan;
+                    END IF;
+                END $$;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                DO $$
+                BEGIN
+                    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'read_randandan') THEN
+                        ALTER DEFAULT PRIVILEGES IN SCHEMA "dbSchemaGoodHamburger" REVOKE SELECT ON TABLES FROM read_randandan;
+                        REVOKE SELECT ON ALL TABLES IN SCHEMA "dbSchemaGoodHamburger" FROM read_randandan;
+                        REVOKE USAGE ON SCHEMA "dbSchemaGoodHamburger" FROM read_randandan;
+                    END IF;
+                END $$;
+                """);
+        }
+    }
+}
